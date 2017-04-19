196e47e9bef23918217d1296510c42300be0313d
Marcos Almeida <marcos.almeida@xcomponent.com>
2017-04-19 17:33:32 +0200

    Issue #2: Adding jenkins build job

---
+++ build.bat
@@ -0,0 +1,30 @@
+echo Running lint...
+pylint ReactiveXComponent -f parseable > pylint.txt
+
+cd ReactiveXComponent
+
+echo Creating virtual environment...
+virtualenv venv
+call venv\Scripts\activate.bat
+
+echo Installing dependencies...
+pip install -r ..\requirements.txt
+IF ERRORLEVEL 1 EXIT /B 1
+
+echo Running tests...
+nosetests tests --with-xunit
+IF ERRORLEVEL 1 EXIT /B 1
+
+echo Checking setup.py...
+python setup.py check
+IF ERRORLEVEL 1 EXIT /B 1
+
+echo Creating source distribution package...
+python setup sdist
+IF ERRORLEVEL 1 EXIT /B 1
+
+echo Leaving virtual environment...
+call venv\Scripts\deactivate.bat
+IF ERRORLEVEL 1 EXIT /B 1
+
+cd ..
