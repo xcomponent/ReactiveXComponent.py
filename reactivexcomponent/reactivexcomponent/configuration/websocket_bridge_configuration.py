14137f08325d24323827179700e9018a84b3b9b7
marwathlithi <marwa.thlithi@xcomponent.com>
2017-05-10 10:27:54 +0200

    issue #3: Update websocket_bridge_configuration file

--- reactivexcomponent/reactivexcomponent/configuration/websocket_bridge_configuration.py
+++ reactivexcomponent/reactivexcomponent/configuration/websocket_bridge_configuration.py
@@ -17,9 +17,11 @@ class WebsocketTopicKind:
 
 class Command:
 
+    # pylint: disable=invalid-name
     hb = "hb"
+    # pylint: enable=invalid-name
 
-    update = "update",
+    update = "update"
 
     subscribe = "subscribe"
 
