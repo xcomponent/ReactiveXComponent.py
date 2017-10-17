309019092b07404ab8b7e2e82a90842132dc3533
nicolaserny <nicolas.erny@xcomponent.com>
2017-10-17 15:52:34 +0200

    Add scripts to build on osx/linux
    + minor fixes

---
+++ autoformat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd reactivexcomponent
+
+autopep8 -r --in-place --recursive --max-line-length=100 -v --exclude venv .
+
+cd ..
--- reactivexcomponent/reactivexcomponent/communication/subscriber.py
+++ reactivexcomponent/reactivexcomponent/communication/subscriber.py
@@ -11,7 +11,7 @@ def get_data_to_send(topic, kind):
             "JsonMessage": json.dumps({
                 "Topic": {"Key": topic, "Kind": kind}
             })
-           }
+            }
 
 
 class Subscriber:
